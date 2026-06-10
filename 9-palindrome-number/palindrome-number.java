2890c43089e3fe5a91ec4c78295c1b249be115a9
Nakul Swami <nakulswami92000@gmail.com>
2026-06-10 12:55:54 +0530

    9-palindrome-number: accepted in Java | Time 4 ms | Memory 46 MB

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,6 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        return s.equals(new StringBuilder(s).reverse().toString());
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 }
