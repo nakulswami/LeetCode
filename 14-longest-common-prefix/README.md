10caad3314bddc12e95ef457727d50384e1d47d6
Nakul Swami <nakulswami92000@gmail.com>
2026-07-03 18:57:09 +0530

    14-longest-common-prefix: accepted in Java | Time 1 ms | Memory 43.4 MB

--- 14-longest-common-prefix/README.md
+++ 14-longest-common-prefix/README.md
@@ -2,5 +2,5 @@
 
 Difficulty: Easy  
 Language: Java  
-Submitted: 2026-07-03T06:14:04.000Z  
+Submitted: 2026-07-03T13:27:02.000Z  
 LeetCode: https://leetcode.com/problems/longest-common-prefix/
--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,16 +1,18 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         Arrays.sort(strs);
-        String str1 = strs[0];
-        String str2 = strs[strs.length-1];
+
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+
         int i = 0;
-        while(i < str1.length() && i <str2.length()){
-            if(str1.charAt(i) == str2.charAt(i)){
+        while(i < s1.length() && i <s2.length()){
+            if(s1.charAt(i) == s2.charAt(i)){
                 i++;
             } else {
                 break;
             }
         }
-        return str1.substring(0, i);
+        return s1.substring(0, i);
     }
 }
