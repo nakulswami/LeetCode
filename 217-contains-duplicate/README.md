f076263e22c69d0d5417b862495e047f13d6cc41
Nakul Swami <nakulswami92000@gmail.com>
2026-07-04 23:07:20 +0530

    217-contains-duplicate: accepted in Java | Time 25 ms | Memory 81.6 MB

--- 217-contains-duplicate/README.md
+++ 217-contains-duplicate/README.md
@@ -2,5 +2,5 @@
 
 Difficulty: Easy  
 Language: Java  
-Submitted: 2026-07-04T17:34:20.000Z  
+Submitted: 2026-07-04T17:37:14.000Z  
 LeetCode: https://leetcode.com/problems/contains-duplicate/
--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,9 +3,8 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i = 1; i < nums.length; i++){
-            if(nums[i] == nums[i -1]){
-                return true;
-            }
+            if(nums[i] == nums[i -1])
+            return true;
         }
         return false;
     }
