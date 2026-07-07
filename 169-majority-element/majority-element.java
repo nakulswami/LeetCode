ae2a8b0111ad8e544f818178c8400f7df119f5be
Nakul Swami <nakulswami92000@gmail.com>
2026-07-07 12:22:53 +0530

    169-majority-element: accepted in Java | Time 7 ms | Memory 55.7 MB

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        for(Map.Entry<Integer, Integer> obj:map.entrySet()){
-            if(obj.getValue() > nums.length/2){
-                return obj.getKey();
-            }
-        }
-        return 0;
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
     }
 }
