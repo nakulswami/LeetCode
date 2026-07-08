55cc813e225c7876a359db14bfef7afec6919bac
Nakul Swami <nakulswami92000@gmail.com>
2026-07-08 20:55:23 +0530

    349-intersection-of-two-arrays: accepted in Java | Time 2 ms | Memory 44.9 MB

--- 349-intersection-of-two-arrays/README.md
+++ 349-intersection-of-two-arrays/README.md
@@ -2,5 +2,5 @@
 
 Difficulty: Easy  
 Language: Java  
-Submitted: 2026-07-08T15:14:38.000Z  
+Submitted: 2026-07-08T15:25:18.000Z  
 LeetCode: https://leetcode.com/problems/intersection-of-two-arrays/
--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,12 +1,21 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> s = new HashSet<>();
-        for (int x : nums1) s.add(x);
+        for (int x : nums1)
+            s.add(x);
 
         Set<Integer> ans = new HashSet<>();
         for (int x : nums2)
-            if (s.contains(x)) ans.add(x);
+            if (s.contains(x))
+                ans.add(x);
 
-        return ans.stream().mapToInt(i -> i).toArray();
+        int[] result = new int[ans.size()];
+        int i = 0;
+
+        for (int x : ans) {
+            result[i++] = x;
+        }
+
+        return result;
     }
 }
