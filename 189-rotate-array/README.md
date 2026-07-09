b23a63b96042e7b3d7bd485443ccb67ffddb4d2d
Nakul Swami <nakulswami92000@gmail.com>
2026-07-09 11:33:55 +0530

    189-rotate-array: accepted in Java | Time 2 ms | Memory 63.3 MB

---
+++ 189-rotate-array/README.md
@@ -0,0 +1,6 @@
+# Rotate Array
+
+Difficulty: Medium  
+Language: Java  
+Submitted: 2026-07-09T06:03:44.000Z  
+LeetCode: https://leetcode.com/problems/rotate-array/
---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int j = (i + k) % n;
+            ans[j] = nums[i];
+        }
+
+        for(int i  = 0; i < n; i++){
+            nums[i] = ans[i];
+        }
+    }
+}
