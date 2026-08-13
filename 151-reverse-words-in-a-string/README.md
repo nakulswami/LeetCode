a0b7de4de3a1467935e443b0977cdc98747608cf
Nakul Swami <nakulswami92000@gmail.com>
2026-08-13 09:56:38 +0530

    151-reverse-words-in-a-string: accepted in Java | Time 7 ms | Memory 44.4 MB

---
+++ 151-reverse-words-in-a-string/README.md
@@ -0,0 +1,6 @@
+# Reverse Words in a String
+
+Difficulty: Medium  
+Language: Java  
+Submitted: 2026-08-13T04:26:31.000Z  
+LeetCode: https://leetcode.com/problems/reverse-words-in-a-string/
---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        for(int i = words.length - 1; i>= 0; i--){
+            result.append(words[i]).append(" ");
+        }
+        return result.toString().trim();
+    }
+}
